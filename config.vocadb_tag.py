7627d3443f4b00fc442499a030becbd2d48a4545
szc126 <>
2020-01-21 09:12:16 -0800

    (config.vocadb_tag): update not important

--- config.vocadb_tag.py
+++ config.vocadb_tag.py
@@ -8,16 +8,16 @@ bom = False # mp3tag
 formatstring_output_file = '.vocadb_tag.formatstring.log'
 
 metadata_tags = {
-	'TITLE': '$title',
-	'ARTIST': '$vocalists',
-	'COMPOSER': '$producers',
-	'DATE': '$year',
-	'GENRE': '$x_synthesizers',
-	'URL': '$url',
-	'COMMENT': '$x_is_reprint | $x_detection_method | $song_type song | $x_db_id@$x_db_name',
 	'__filename_ext': '$x_filename_ext', # mp3tag
+	'x_title': lambda x: x['title'] + (' (feat. ' + ' ,'.join(x['vocalists_support']) + ')' if x['vocalists_support'] else ''),
+	'x_artist': '$vocalists',
+	'x_composer': '$composers',
+	'date': '$year',
+	'genre': '$x_vocalist_types',
+	'url': '$url',
+	'x_comment': '$x_is_reprint | $x_detection_method | $song_type song | $x_db_id@$x_db_name',
 }
-metadata_multi_value_delimiter = '\\\\' # as in "初音ミク; GUMI"
+metadata_multi_value_delimiter = '#' # as in "初音ミク; GUMI"
 metadata_empty_placeholder = '-' # foobar2000
 
 ffprobe = True # True, False, r"D:\bin\ffmpeg\ffprobe.exe"
