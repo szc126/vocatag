5eeaa8e299ab8414c3f1c9dfc003e3c339c58edf
szc126 <29127915+szc126@users.noreply.github.com>
2021-09-19 18:00:23 -0700

    config.vocadb_tag.py: large update

--- config.vocadb_tag.py
+++ config.vocadb_tag.py
@@ -2,22 +2,39 @@
 
 language = 'Default' # Default, Japanese, Romaji, English
 
-tags_output_file = '.vocadb_tag.log'
-tags_output_file_tag_delimiter = ' #~~# '
+tags_output_file = '/tmp/vocadb_tag.log'
+tags_output_file_tag_delimiter = '＄'
 bom = False # mp3tag
-formatstring_output_file = '.vocadb_tag.formatstring.log'
+formatstring_output_file = '/tmp/vocadb_tag.formatstring.log'
 
 metadata_tags = {
 	'__filename_ext': '$x_filename_ext', # mp3tag
-	'x_title': lambda x: x['title'] + (' (feat. ' + ' ,'.join(x['vocalists_support']) + ')' if x['vocalists_support'] else ''),
-	'x_artist': '$vocalists',
-	'x_composer': '$composers',
+	'title': lambda x: x['title'] + (' (feat. ' + ', '.join(x['vocalists_support']) + ')' if x['vocalists_support'] else ''),
+	'artist': '$vocalists',
+	'composer': lambda x: '' + ';'.join(
+		[s for s in [
+			('カバー' if x['song_type'] == 'Cover' and not 'Utaite' in x['x_vocalist_types'] else None),
+			('歌ってみた' if x['song_type'] == 'Cover' and 'Utaite' in x['x_vocalist_types'] else None),
+		] if s] +
+		x['composers']
+	),
 	'date': '$year',
-	'genre': '$x_vocalist_types',
+	'genre': lambda x: ';'.join(
+		[s for s in [
+			('S:Re' if x['x_is_reprint'] else None),
+			'V' + ('アレンジ' if x['song_type'] == 'Remix' else ''),
+		] if s] +
+		list(x['x_vocalist_types'].keys())
+	)
+		.replace('Producer', 'ヒト')
+		.replace('Utaite', 'ヒト')
+		.replace('OtherVocalist', 'ヒト')
+		.replace('Vocaloid', 'VOCALOID')
+	,
 	'url': '$url',
-	'x_comment': '$x_is_reprint | $x_detection_method | $song_type song | $x_db_id@$x_db_name',
+	'comment': '$song_type song | $x_db_id@$x_db_name',
 }
-metadata_multi_value_delimiter = '#' # as in "初音ミク; GUMI"
+metadata_multi_value_delimiter = ';' # as in "初音ミク; GUMI"
 metadata_empty_placeholder = '-' # foobar2000
 
 ffprobe = True # True, False, r"D:\bin\ffmpeg\ffprobe.exe"
